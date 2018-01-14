9d6eef793b102766b71adfae1129a79383a4b892
jrior001 <jriordan001@gmail.com>
2018-01-13 20:10:39 -0500

    charger: remove duplicate charger_res_images
    Change-Id: Iaef862409eaf11fb5f079daf04be3420e268ca5b

--- bacon.mk
+++ bacon.mk
@@ -43,10 +43,6 @@ PRODUCT_PACKAGES += \
     libshim_camera \
     libshim_cameraclient
 
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images
-
 # Dalvik/HWUI
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)

bd10dbfbf81ab7c8b73e471f55db58cc2854305b
jrior001 <jriordan001@gmail.com>
2018-01-13 20:30:21 -0500

    use common seccomp policy
    Change-Id: I9e4fac7bba9645fce09d5e18542566d8211b9ae9

--- bacon.mk
+++ bacon.mk
@@ -82,11 +82,6 @@ PRODUCT_PACKAGES += \
     libinit_bacon \
     ueventd.bacon.rc
 
-# Seccomp
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
